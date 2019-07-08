c0a1578fcd71f4f219d3548b7d9a3ba73a6f78fc
Taka Shirono <takashirono@gmail.com>
2019-07-08 10:18:19 -0400

    build basic framework to display pre-populated boxedit page for editing box

--- util_box_app/lib/screens/box_admin.dart
+++ util_box_app/lib/screens/box_admin.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/box_info.dart';
+import '../models/box_info.dart';
+import '../screens/box_create.dart';
 
 // class BoxAdminPage extends StatefulWidget {
 //   @override
@@ -9,15 +14,30 @@ import 'package:flutter/material.dart';
 
 class BoxAdminPage extends StatelessWidget {
   static const String pageRoute = 'adminBox';
-  
+
   @override
   Widget build(BuildContext context) {
+    final BoxInfoProvider boxData = Provider.of<BoxInfoProvider>(context);
+    List<BoxInfo> boxInfos = boxData.boxInfos;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Testing - Admin Page"),
       ),
-      body: Center(
-        child: Text("Test - Admin"),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: boxInfos.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(boxInfos[index].boxTitle),
+            leading: Icon(boxInfos[index].iconData),
+            trailing: Icon(Icons.edit),
+            onTap: () {
+              Navigator.of(context).pushNamed(BoxCreatePage.pageRoute, arguments: boxInfos[index]);
+            },
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
     );
   }
